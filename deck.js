2f577bb64b12b65c6e7a3a657abf3776b926bda2
DavidSosaMoriana <sosamorianadav@gmail.com>
2022-07-10 10:30:27 +0100

    Finish Game

--- deck.js
+++ deck.js
@@ -1,18 +1,18 @@
 const SUITS = ["♠", "♣", "♥", "♦"]
 const VALUES = [
-"A", 
-"2", 
-"3", 
-"4", 
-"5",
-"6",
-"7",
-"8",
-"9", 
-"10", 
-"J", 
-"Q", 
-"K"
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
 ]
 
 export default class Deck {
@@ -24,10 +24,18 @@ export default class Deck {
     return this.cards.length
   }
 
+  pop() {
+    return this.cards.shift()
+  }
+
+  push(card) {
+    this.cards.push(card)
+  }
+
   shuffle() {
     for (let i = this.numberOfCards - 1; i > 0; i--) {
       const newIndex = Math.floor(Math.random() * (i + 1))
-      const oldValue = this.cards [newIndex]
+      const oldValue = this.cards[newIndex]
       this.cards[newIndex] = this.cards[i]
       this.cards[i] = oldValue
     }
@@ -39,6 +47,18 @@ class Card {
     this.suit = suit
     this.value = value
   }
+
+  get color() {
+    return this.suit === "♣" || this.suit === "♠" ? "black" : "red"
+  }
+
+  getHTML() {
+    const cardDiv = document.createElement("div")
+    cardDiv.innerText = this.suit
+    cardDiv.classList.add("card", this.color)
+    cardDiv.dataset.value = `${this.value} ${this.suit}`
+    return cardDiv
+  }
 }
 
 function freshDeck() {
@@ -46,5 +66,5 @@ function freshDeck() {
     return VALUES.map(value => {
       return new Card(suit, value)
     })
-  })  
+  })
 }
